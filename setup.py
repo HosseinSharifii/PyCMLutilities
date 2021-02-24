69c076ba15f349eec6d7982716fb1b0e601e5a13
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-02-24 14:15:14 -0500

    version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyCMLutil-Hossein_pypi", # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Hossein Sharifi",
     author_email="hossein.sharifi@uky.edu",
     description="A usefule python package for creating sientific figures ",

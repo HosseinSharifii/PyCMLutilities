62b2607b86b9727b7b5516da8ab38a308b44d598
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-07-19 10:45:15 -0400

    categorical demos

---
+++ docs/pages/demos/multipanel_numerical/numerical.md
@@ -0,0 +1,10 @@
+---
+title: Multipanel Numerical
+parent: Demos
+has_children: False
+nav_order: 1
+---
+
+# Multipanel Numerical
+
+Hossein was here
--- test/test_cat.py
+++ test/test_cat.py
@@ -3,20 +3,23 @@ import pandas as pd
 
 
 def test_cat():
-    data_file_string = "data/valvular_disease.xlsx"
-    temp_list = ['json/1X1_strip_valvular.json',
-                'json/1X1_strip_hue_valvular.json',
-                'json/1X1_point_valvular.json',
-                'json/1X1_point_strip_valvular.json',
-                'json/1X1_point_hue_valvular.json',
-                'json/1X1_box_valvular.json',
-                'json/1X1_box_strip_valvular.json',
-                'json/1X1_box_hue_valvular.json',
-                'json/4X4_multiple_valvular.json']
-    temp_list = ['json/1X1_point_valvular.json']
+    data_file_string = "demos/data/valvular_disease.xlsx"
+    temp_list = ['demos/templates/1X1_strip_valvular.json',
+                'demos/templates/1X1_strip_valvular_markers.json',
+                'demos/templates/1X1_strip_valvular_no_jitter.json',
+                'demos/templates/1X1_strip_valvular_hue.json',
+                'demos/templates/1X1_point_valvular.json',
+                'demos/templates/1X1_point_valvular_markers.json',
+                'demos/templates/1X1_point_valvular_hue.json',
+                'demos/templates/1X1_box_valvular.json',
+                'demos/templates/1X1_box_valvular_hue.json',
+                'demos/templates/1X1_box_valvular_modified_style.json',
+                'demos/templates/1X1_box_strip_valvular.json',
+                'demos/templates/1X2_box_point_strip_valvular.json',
+                'demos/templates/4X4_multiple_valvular.json']
     for i,t in enumerate(temp_list):
         print(i)
-        output_image_str = 'examples/'+'test_'+t.split('.')[0].split('/')[-1]+'.png'
+        output_image_str = 'demos/categorical/'+t.split('.')[0].split('/')[-1]+'.png'
 
 
         mplc(data_file_string = data_file_string,

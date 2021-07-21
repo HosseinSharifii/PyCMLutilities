b3b4353a2be46d7b6b54a3ddf08798c928a71bee
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-07-21 17:02:03 -0400

    test numerical plots

--- test/test_mpl.py
+++ test/test_mpl.py
@@ -3,16 +3,42 @@ import pandas as pd
 
 
 def test_mpl():
-    data_file_string = "data/pymyovent_test.csv"
-    temp_list = ['templates/pymyovent.json']
+    data_file_string = "demos/data/test_data.xlsx"
+    temp_list = [None,
+                "demos/templates/1X1_mpl.json",
+                "demos/templates/1X1_wide.json",
+                "demos/templates/1X1_x_display.json",
+                "demos/templates/1X1_height.json",
+                "demos/templates/1X1_x_ticks.json",
+                "demos/templates/1X2_wide.json",
+                "demos/templates/1X3_wide.json",
+                "demos/templates/1X3_wide_scaling.json",
+                "demos/templates/1X2_envelope.json",
+                "demos/templates/2X2_partial.json",
+                "demos/templates/2X2_full.json",
+                "demos/templates/2X2_multi_series.json",
+                "demos/templates/2X2_multi_series_labels.json",
+                "demos/templates/2X2_envelope_partial.json",
+                "demos/templates/2X2_mix.json",
+                "demos/templates/5X2_mpl.json",
+                "demos/templates/5X2_margins.json",
+                "demos/templates/2X2_mix_annotate.json",
+                "demos/templates/pymyovent.json"]
+
     for i,t in enumerate(temp_list):
         print(i)
-        output_image_str = 'examples/'+'test_'+t.split('.')[0].split('/')[-1]+'.png'
+        if t == None:
+            output_image_str = 'demos/numerical/'+'no_temp'+'.png'
+        elif t.split('/')[-1].split('.')[0] == 'pymyovent':
+            data_file_string = "demos/data/pymyovent_test.csv"
+            output_image_str = 'demos/numerical/'+t.split('.')[0].split('/')[-1]+'.png'
+        else:
+            output_image_str = 'demos/numerical/'+t.split('.')[0].split('/')[-1]+'.png'
 
 
         mpl(data_file_string = data_file_string,
             template_file_string = t,
-            output_image_file_string = output_image_str,dpi=100)
+            output_image_file_string = output_image_str)
 
 if __name__ == "__main__":
     test_mpl()

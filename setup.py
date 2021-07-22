f86fbea7a54b9bf5e858bc00615771668da2c1f7
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-07-22 11:24:37 -0400

    test pypi rep

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyCMLutil-Hossein_pypi", # Replace with your own username
-    version="0.0.12",
+    name="PyCMLutil", # Replace with your own username
+    version="0.0.1",
     author="Hossein Sharifi",
     author_email="hossein.sharifi@uky.edu",
-    description="A usefule python package for creating sientific figures ",
+    description="A usefule python package for generating sientific figures.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Campbell-Muscle-Lab/PyCMLutilities",

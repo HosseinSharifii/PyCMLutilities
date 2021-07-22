2ea893ca3ed5d999b3e97c8412840bea2c92aa83
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-07-22 11:19:17 -0400

    setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Campbell-Muscle-Lab/PyCMLutilities",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests","demos")),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
